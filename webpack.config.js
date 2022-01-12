a14cb7f3fcfebe73c765d3b587e1ccf4272af410
James Chung <sunhochung53@gmail.com>
2022-01-11 16:00:43 -0800

    Set up server files and bundle server/client

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.join(__dirname, 'client'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
