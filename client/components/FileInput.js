e6963ad12ac973da1d9f491c1ba086bd9862c960
James Chung <sunhochung53@gmail.com>
2022-01-14 00:48:11 -0800

    Fix thumbnail generation with correct chaining

--- client/components/FileInput.js
+++ client/components/FileInput.js
@@ -1,59 +1,41 @@
 import React, { Component } from 'react'
-
-const MAX_WIDTH = 400;
-const MAX_HEIGHT = 400;
+import { uploadImage } from '../features/inventory/inventoryAPI';
+import { resize } from '../util/image'
 
 export default class FileInput extends Component {
-
     _readURL = (file) => {
-        console.log(file); // !
         return new Promise((res, rej) => {
             const reader = new FileReader();
-            reader.onload = (e) => { alert(e.target.result.width + " " + this.height); res(e.target.result) };
+            reader.onload = (e) => { res(e.target.result) };
             reader.onerror = (e) => { rej(e) };
             reader.readAsDataURL(file);
         });
     }
 
     render() {
+        var img = new Image();
         var canvas = document.createElement('canvas');
 
         const setImage = async (event) => {
             const file = event.target.files[0];
-            console.log(file); // !
             const url = await this._readURL(file);
 
-            var img = document.createElement('img');
+            img.addEventListener('load', () => {
+                var newDimensions = resize(img.naturalWidth, img.naturalHeight)
+
+                canvas.width = newDimensions.width;
+                canvas.height = newDimensions.height;
+                var ctx = canvas.getContext('2d');
+                ctx.drawImage(img, 0, 0, newDimensions.width, newDimensions.height);
+            });
+
             img.src = url;
-            console.log(img); // !
-            document.body.appendChild(img)
-
-            var width = img.width;
-            var height = img.height;
-
-            if (width > height) {
-                if (width > MAX_WIDTH) {
-                    height *= MAX_WIDTH / width;
-                    width = MAX_WIDTH;
-                }
-            } else {
-                if (height > MAX_HEIGHT) {
-                    width *= MAX_HEIGHT / height;
-                    height = MAX_HEIGHT;
-                }
-            }
-
-            canvas.width = width;
-            canvas.height = height;
-            var ctx = canvas.getContext('2d');
-            ctx.drawImage(img, 0, 0, width, height);
-            console.log(canvas); // !
         }
 
         const handleSubmit = () => {
             canvas.toBlob((blob) => {
-                let file = new File([blob], 'fileName.jpg', { type: "image/jpeg" });
-                console.log(file);
+                let resizedFile = new File([blob], 'fileName.jpg', { type: "image/jpeg" });
+                uploadImage(resizedFile);
             }, 'image/jpeg');
         }
 
--- client/util/image.js
+++ client/util/image.js
@@ -1,67 +1,23 @@
-import { uploadImage } from '../features/inventory/inventoryAPI'
+const MAX_WIDTH = 400;
+const MAX_HEIGHT = 400;
 
-// Modified from source: https://hacks.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
-// Creates a new canvas element, resizes given image, and returns as Blob
-export const generateThumbnail = async (file) => {
-    var fr = new FileReader();
+export const resize = (width, height) => {
+    var newDimensions = {
+        width: width,
+        height: height
+    }
 
-    fr.addEventListener('load', () => {
-        var img = new Image();
-        var canvas = document.createElement("canvas");
-        img.src = fr.result;
-        console.log(img);
-
-        var MAX_WIDTH = 400;
-        var MAX_HEIGHT = 400;
-        var width = img.width;
-        var height = img.height;
-        console.log('Dimensions: ', width, height)
-
-        if (width > height) {
-            if (width > MAX_WIDTH) {
-                height *= MAX_WIDTH / width;
-                width = MAX_WIDTH;
-            }
-        } else {
-            if (height > MAX_HEIGHT) {
-                width *= MAX_HEIGHT / height;
-                height = MAX_HEIGHT;
-            }
+    if (width > height) {
+        if (width > MAX_WIDTH) {
+            newDimensions.height *= MAX_WIDTH / width;
+            newDimensions.width = MAX_WIDTH;
         }
+    } else {
+        if (height > MAX_HEIGHT) {
+            newDimensions.width *= MAX_HEIGHT / height;
+            newDimensions.height = MAX_HEIGHT;
+        }
+    }
 
-        canvas.width = width;
-        canvas.height = height;
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, width, height);
-        console.log(canvas);
-
-        canvas.toBlob((blob) => {
-            let file = new File([blob], "fileName.jpg", { type: "image/jpeg" });
-            console.log(JSON.stringify(file));
-        }, 'image/jpeg');
-    }, false);
-
-    fr.readAsDataURL(file);
+    return newDimensions;
 }
-
-const preview = async (event) => {
-    const file = event.target.file //CHANGE
-    const url = await readURL(file);
-    img.src = url;
-}
-
-const readURL = (file) => {
-    return new Promise((res, rej) => {
-        const reader = new FileReader();
-        reader.onload = (e) => { res(e.target.result) };
-        reader.onerror = (e) => { rej(e) };
-        reader.readAsDataURL(file);
-    });
-}
-
-const fileInput = document.createElement('input');
-fileInput.type = 'file';
-const img = document.createElement('img');
-img.attributeStyleMap.set('max-width', '320px');
-
-fileInput.addEventListener('change', preview)
